80d13a58d0c00c800c3b97ad5e8a62bdaa4b3bd3
Chris Wiederspan <chris@wiederspan.com>
2018-09-17 17:56:25 -0600

    Docker and build.yaml file changes

--- MicroService.Api/Program.cs
+++ MicroService.Api/Program.cs
@@ -8,17 +8,18 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace MicroService.Api
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+namespace MicroService.Api {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            WebHost
+              .CreateDefaultBuilder(args)
+              .UseStartup<Startup>();
     }
 }
