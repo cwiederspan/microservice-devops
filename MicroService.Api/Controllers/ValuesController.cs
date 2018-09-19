487dce5259c395da26cc2ffdec553131b7d7bc49
Chris Wiederspan <chris@wiederspan.com>
2018-09-18 19:01:14 -0600

    Updated values

--- MicroService.Api/Controllers/ValuesController.cs
+++ MicroService.Api/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace MicroService.Api.Controllers {
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get() {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "value3" };
         }
 
         // GET api/values/5
